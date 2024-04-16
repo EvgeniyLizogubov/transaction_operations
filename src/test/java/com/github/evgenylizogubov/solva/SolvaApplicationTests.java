28e0801121746809858a15f02092306a6813a319
Lizogubov Eugeny <noobikrus@gmail.com>
2024-04-16 09:38:47 +0500

    Init commit

---
+++ src/test/java/com/github/evgenylizogubov/solva/SolvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.evgenylizogubov.solva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvaApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
